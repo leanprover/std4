08b7216e09c5e6470ca1e65c92ce4673525c5dda
Kyle Miller <kmill31415@gmail.com>
2024-06-23 17:17:06 -0700

    fixes, deleted `Function.funext_iff`

--- Batteries/Data/String/Lemmas.lean
+++ Batteries/Data/String/Lemmas.lean
@@ -11,7 +11,8 @@ import Batteries.Tactic.SeqFocus
 
 namespace String
 
-attribute [ext] ext
+-- TODO(kmill): add `@[ext]` attribute to `String.ext` in core.
+attribute [ext (iff := false)] ext
 
 theorem lt_trans {s₁ s₂ s₃ : String} : s₁ < s₂ → s₂ < s₃ → s₁ < s₃ :=
   List.lt_trans' (α := Char) Nat.lt_trans
@@ -86,7 +87,8 @@ end
 
 namespace Pos
 
-attribute [ext] ext
+-- TODO(kmill): add `@[ext]` attribute to `String.Pos.ext` in core.
+attribute [ext (iff := false)] ext
 
 theorem lt_addChar (p : Pos) (c : Char) : p < p + c := Nat.lt_add_of_pos_right (Char.utf8Size_pos _)
 
